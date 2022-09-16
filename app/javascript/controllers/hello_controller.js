ebca6e325a9bbf061121a2944baac43a5548612e
Martim Correia <correia_martim@hotmail.com>
2022-09-16 17:33:57 +0100

    flip card

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,7 +1,13 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static targets = ["flip"]
   connect() {
-    this.element.textContent = "Hello World!"
+
+    // this.element.textContent = "Hello World!"
   }
+  disable() {
+    this.flipTarget.classList.toggle("is-flipped")
+  }
+
 }
