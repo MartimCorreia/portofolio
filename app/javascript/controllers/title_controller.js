e408c75fa0cad530ba948a9f4226915b3dc41fdb
Martim Correia <correia_martim@hotmail.com>
2022-09-24 17:21:42 +0100

    fadein p about me

--- app/javascript/controllers/title_controller.js
+++ app/javascript/controllers/title_controller.js
@@ -1,17 +1,16 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["bganimate"]
+  static targets = ["bganimate", "paragraph"]
   connect() {
     const img = document.getElementsByTagName('img');
 
     window.addEventListener('load', () => {
       img[0].className = "showingup";
-      this.bganimateTarget.className = "about-frame-in d-flex justify-content-start"
+      this.bganimateTarget.className = "about-frame-in d-flex justify-content-start";
+      this.paragraphTarget.className = "showit";
     });
-    window.addEventListener('loadend', () =>{
-      
-    });
+
 
   }
 }
