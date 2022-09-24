3e8db4abdbde877c8b86f03072f54f936d9e4f83
Martim Correia <correia_martim@hotmail.com>
2022-09-24 17:01:38 +0100

    another simple JS event animation

--- app/javascript/controllers/title_controller.js
+++ app/javascript/controllers/title_controller.js
@@ -1,11 +1,13 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static targets = ["bganimate"]
   connect() {
     const img = document.getElementsByTagName('img');
 
     window.addEventListener('load', () => {
       img[0].className = "showingup";
+      this.bganimateTarget.className = "about-frame-in d-flex justify-content-start"
     });
 
   }
