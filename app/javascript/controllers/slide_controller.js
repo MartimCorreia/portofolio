e87856f4a45d6f4d3945615680527e76a62d88a0
Martim Correia <correia_martim@hotmail.com>
2022-09-29 17:59:28 +0100

    contact page animations dealt with

--- app/javascript/controllers/slide_controller.js
+++ app/javascript/controllers/slide_controller.js
@@ -3,17 +3,14 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
 static targets = ["zone"];
   connect() {
-
-
     const imageCount = $('#slider ul li').length;
     const imageWidth = $('#slider ul li img').first().width();
     const totalWidth = (imageWidth + imageCount) + 'px';
+    const slideEnabler = [1, 1];
+
 
     let leftPosition = 0;
     let counter = 0;
-
-
-
     $('#slider ul').css('width', totalWidth);
 
 
@@ -30,21 +27,35 @@ static targets = ["zone"];
           $('#slider ul').first().remove();
         });
         counter = 0;
-
-
       }
       else {
         $('#slider ul').animate( {left: leftPosition }, 1000, 'easeInQuad');
       }
     };
 
+     let timer = setInterval(sliding, 2500);
 
-  let timer = setInterval(sliding, 2500);
+
 
 
     document.getElementById('slider').addEventListener('mouseover',function() {
+
       clearInterval(timer);
+      // console.log($('#slider ul').css('left'))
+      if($('#slider ul').css('left') == "-200px"){
 
+        $('#slider ul').animate( {left: "-210px"}, 500);
+        $('#slider ul li img').animate( {width: "210px", height: "210px"}, 500 );
+        $('#slider').animate( {width: "210px", height: "210px" }, 500 );
+        $('#tab2').fadeIn(500);
+
+      }
+      else if($('#slider ul').css('left') == "0px" ){
+        console.log('im here')
+        $('#slider ul li img').animate( {width: "210px", height: "210px"}, 500 );
+        $('#slider').animate( {width: "210px", height: "210px" }, 500 );
+        $('#tab1').fadeIn(500);
+      }
     });
 
 
@@ -52,12 +63,28 @@ static targets = ["zone"];
 
 
 
-    document.getElementById('slider').addEventListener('mouseout',function() {
-      timer = setInterval(sliding, 2500);
+    document.getElementById('slider').addEventListener('click',function() {
+
+        timer = setInterval(sliding, 2500);
+
+
+
+      if( $('#slider ul').css('left') == "-210px"){
+        $('#slider ul').animate( {left: "-200px"}, 500);
+        $('#slider ul li img').animate( {width: "200px", height: "200px"}, 500 );
+        $('#slider').animate( {width: "200px", height: "200px" }, 500 );
+        $('#tab2').fadeOut(500);
+      }
+      else{
+        $('#slider ul li img').animate( {width: "200px", height: "200px"}, 500 );
+        $('#slider').animate( {width: "200px", height: "200px" }, 500 );
+        $('#tab1').fadeOut(500);
+      }
 
     });
+
+
+
 
   }
-
-
 }
